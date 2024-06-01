1f6bfbac5745b07b91904a4d7e8deea4ba1390d8
Kevin-gram <k.nyiringan@alustudent.com>
2024-06-01 21:48:11 +0200

    Derive happiness in oneself from a good day's work

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,1 +1,15 @@
-3
+#!/usr/bin/env python3
+"""
+This module provides a function to calculate,
+the sum of squares,
+of the first n natural numbers.
+"""
+
+
+def summation_i_squared(n):
+    """
+    Calculate the sum of squares of the first n natural numbers.
+    """
+    if not isinstance(n, int) or n < 1:
+        return None
+    return n * (n + 1) * (2 * n + 1) // 6
