d85d4dab3564966641898263dfc379cc22ae0784
Kevin_Nyiringango <116556392+Kevin-gram@users.noreply.github.com>
2024-10-26 16:17:12 +0200

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
-""" Prints the location of a GitHub user specified
-as an arg to the script
-"""
-import sys
+
+
+""" Return list of ships"""
+
 import requests
-import json
+import sys
+import time
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    response = requests.get(url)
 
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        print(data['location'])
-    elif response.status_code == 403:
-        print('Reset in ')
-    elif response.status_code == 404:
-        print('Not found')
-    else:
-        print('Error:', response.status_code)
+if __name__ == "__main__":
+    res = requests.get(sys.argv[1])
+
+    if res.status_code == 403:
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
+        current_time = int(time.time())
+        diff = (rate_limit - current_time) // 60
+        print("Reset in {} min".format(diff))
+        
+
+    elif res.status_code == 404:
+        print("Not found")
+    elif res.status_code == 200:
+        res = res.json()
+        print(res['location'])
